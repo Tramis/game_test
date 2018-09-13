be25a0f04ba4ec1ed4d17f6aad2b08118a1cb447
Tramis <34150794+Tramis@users.noreply.github.com>
2018-09-13 17:22:32 +0800

    sth new

--- codee/main_test.py
+++ codee/main_test.py
@@ -4,9 +4,12 @@ from sys import exit
 
 pygame.init()
 
-winSize = [640, 480]
+winSize = [1200, 800]
 potInit = (0, 0)
 potCar = [10, 10]
+dropTime = 0
+accTime = 0
+speed = 3
 moveDict = {pygame.K_LEFT: 0, pygame.K_RIGHT: 0, pygame.K_UP: 0, pygame.K_DOWN: 0}
 
 threeCar1 = pygame.image.load('img/twr1.png')
@@ -18,7 +21,7 @@ strOut = 'out'
 
 win = pygame.display.set_mode(winSize)
 pygame.display.set_caption('今晚，我在秋名山等你')
-backGround = pygame.image.load('img/bg.jpg')
+backGround = pygame.image.load('img/bg2.jpg')
 
 def showCar(flag):
     if flag[0] == 1000:
@@ -30,12 +33,36 @@ def showCar(flag):
     flag[0] += 1
 
 def moveObject(pot):
+    global dropTime
+    moveDict[pygame.K_UP] = 0
     pot[0] += (moveDict[pygame.K_RIGHT] - moveDict[pygame.K_LEFT])
     pot[1] += (moveDict[pygame.K_DOWN] - moveDict[pygame.K_UP])
     if pot[0] < 0:
         pot[0] = 0
     if pot[1] < 0:
         pot[1] = 0
+    if pot[0] > 1000:
+        pot[0] = 1000
+    if pot[1] > 680:
+        pot[1] = 680
+        dropTime = 0
+    if pot[1] < 680:
+        pot[1] += 5
+    if dropTime > 20:
+        pot[1] -= dropTime/2
+        dropTime -= 3
+    elif dropTime > 0:
+        pot[1] += 3
+
+def jump(pot):
+    global dropTime
+    if pot[1] == 680:
+         dropTime = 50
+
+def acc():
+    global accTime, speed
+    speed = 5
+    accTime = 100
 
 def showText(text):
     font = pygame.font.Font('freesansbold.ttf', 16)
@@ -57,10 +84,18 @@ while isCon:
 
         if i.type == pygame.KEYDOWN:
             if i.key in moveDict:
-                moveDict[i.key] = 10
+                moveDict[i.key] = speed
+            if i.key == pygame.K_SPACE:
+                jump(potCar)
+            if i.key == pygame.K_LCTRL:
+                acc()
 
         if i.type == pygame.KEYUP:
             if i.key in moveDict:
                 moveDict[i.key] = 0
 
     moveObject(potCar)
+    if accTime == 0:
+        speed = 3
+    else:
+        accTime -= 1
